af4277c075f3e72e3f1cc2cf36ad596bd7ed052b
Amith <amith@cadrac.org>
2018-09-20 17:16:07 +0530

    this is initial commit for this

---
+++ mylibrary/src/main/java/com/novisync/mylibrary/test.java
@@ -0,0 +1,11 @@
+package com.novisync.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class test {
+    public static void s(Context c,String ss)
+    {
+        Toast.makeText(c, ss, Toast.LENGTH_SHORT).show();
+    }
+}
